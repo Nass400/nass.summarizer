1452233d5204e0c4ff910ad2f3a60b3de7491002
Nassim <nassimnabi@outlook.com>
2023-04-23 11:05:55 +0100

    secound commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <button
           type='button'
           onClick={() =>
-            window.open("https://github.com/nass400/nass.summerizer", "_blank")
+            window.open("https://github.com/Nass400/nass.summarizer", "_blank")
           }
           className='black_btn'
         >
